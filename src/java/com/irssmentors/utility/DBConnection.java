8290249aa57f4e8a8c6da06e4903dd71894dd00b
Kyran-Git <niklauscareer@gmail.com>
2025-12-14 03:30:02 +0800

    - created a package to store utility or static files such as the database connection as a standalone file

---
+++ src/java/com/irssmentors/utility/DBConnection.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.irssmentors.utility;
+
+/**
+ *
+ * @author nikla
+ */
+public class DBConnection {
+    
+}
