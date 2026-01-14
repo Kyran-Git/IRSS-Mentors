75864d1c810ae1710305c53b5abede26e7015314
[IkhGitHub] <[ikhwanyunus648@gmail.com]>
2026-01-15 07:41:48 +0800

    Merge origin/master

--- src/java/com/irssmentors/model/Mentee.java
+++ src/java/com/irssmentors/model/Mentee.java
@@ -5,10 +5,30 @@
  */
 package com.irssmentors.model;
 
+import java.io.Serializable;
+
 /**
  *
  * @author nikla
  */
-public class Mentee {
+public class Mentee implements Serializable{
     
+    private String username;
+    private String password;
+    
+    public Mentee(){
+        
+    }
+    
+    public Mentee(String username, String password){
+        this.username = username;
+        this.password=password;
+    }
+    
+    public String getUsername(){
+        return username;
+    }
+    public String getPassword(){
+        return password;
+    }
 }
